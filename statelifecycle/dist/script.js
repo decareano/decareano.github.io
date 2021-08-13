da547884668f9604624eeaa00c21b68d8921c862
Marcelo Gobelli <almagro@comcast.net>
2021-08-13 07:11:43 -0700

    state with class
    state sample with dates

--- statelifecycle/dist/script.js
+++ statelifecycle/dist/script.js
@@ -1,29 +1,45 @@
-function MyComponent(props) {
-  const someString = "something in the air";
-  return /*#__PURE__*/(
-    React.createElement("div", { className: "class1 class2" }, /*#__PURE__*/
+// From here: 1.
 
-    React.createElement("h2", null, "Hello, ", props.planet), /*#__PURE__*/
-    React.createElement("p", null, someString), /*#__PURE__*/
-    React.createElement("p", null, 18 + 4)));
-
-
-
-}
-const element1 = /*#__PURE__*/React.createElement(MyComponent, { planet: "Tierra" });
-ReactDOM.render(
-element1,
-document.getElementById('root'));
-
-
-
-
-
-// function Welcome(props) {
-//   return <h1>Hello, {props.name}</h1>
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+//   );
 // }
-// const element = <Welcome name="Sara" />
-// ReactDOM.render(
-//   element,
-//   document.getElementById('root')
-// )
+
+// setInterval(tick, 5000);
+
+// to here: 2. 
+// I see two functions: One Clock with props and the other 
+// tick function as a regular function (ie: not component with react-DOM method in it)
+
+// encapsulate the clock. dont know what that is but I play
+// Clock was a function now it's a class
+// date will be moved from props to state....stay tune...
+// this.props gets replaced by this.state
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+
+  }
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("div", null, /*#__PURE__*/
+      React.createElement("h1", null, "Hello, world and back!"), /*#__PURE__*/
+      React.createElement("h2", null, "It is ", this.state.date.toLocaleTimeString(), ".")));
+
+
+  }}
+
+
+ReactDOM.render( /*#__PURE__*/
+React.createElement(Clock, null), //this is redundant. it should be just <Clock />
+// how do we do it? with STATE...
+document.getElementById('root'));
